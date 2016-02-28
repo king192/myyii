a2e2cf60ae78af7022d937495615dc52d650f7d8
xhq <1434970057@qq.com>
2016-02-28 12:07:52 +0800

    http cache

---
+++ controllers/HttpController.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+
+class HttpController extends Controller{
+	public function _init(){
+
+	}
+	public function actionIndex(){
+		return $this->renderPartial('index');
+		// var_dump(get_class_methods($this));
+	}
+}
