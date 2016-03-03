13fd5bec049eec3365bbf458b0015ad648d6e58b
xhq <1434970057@qq.com>
2016-03-03 09:57:07 +0800

    csrf

---
+++ controllers/CsrfController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\controllers;
+
+/*
+防止攻击方法：
+1.开启验证码
+2.请求头refer
+3.
+*/
+class CsrfController extends yii\web\Controller{
+	public function actionIndex(){}
+}
--- controllers/XssController.php
+++ controllers/XssController.php
@@ -2,14 +2,28 @@
 
 namespace app\controllers;
 
+/*
+<img src=x onerror="alert(3);"/>
+<a href="javascript:alert(true);"></a>
+
+
+
+
+*/
 class XssController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-    	// \YII::$app->response->headers->add('x-xss-protection',0);//告诉浏览器正常执行javascript
+    	\YII::$app->response->headers->add('x-xss-protection',0);//告诉浏览器正常执行javascript
     	//$s = \YII::$app->request->get('s');
     	if(isset($_GET['s'])){
         	echo $_GET['s'];
+            echo "\n<br>==========<br>\n";
+            //过滤标签及标签内的内容
+            echo \yii\helpers\HtmlPurifier::process($_GET['s']);
+            echo "\n<br>==========<br>\n";
+            //编码html标签
+            echo \yii\helpers\Html::encode($_GET['s']);
     	}
     }
     /*escape("&")//%26
